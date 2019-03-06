44ed2578ffb6f5235403a2127ec7df1030414eb8
danhuyle508 <38339026+danhuyle508@users.noreply.github.com>
2019-03-05 20:19:18 -0800

    Update test_fifo_animal_shelter.py

--- fifo_animal_shelter/test_fifo_animal_shelter.py
+++ fifo_animal_shelter/test_fifo_animal_shelter.py
@@ -1,47 +1,47 @@
 from fifo_animal_shelter import Queue
 
-# def test_enqueue_one():
-#     shelter = Queue()
+def test_enqueue_one():
+    shelter = Queue()
 
-#     shelter.enqueue({'type':'dog', 'name':'fido'})
+    shelter.enqueue({'type':'dog', 'name':'fido'})
 
-#     assert shelter.tail.value['type'] == 'dog'
+    assert shelter.tail.value['type'] == 'dog'
 
-# def test_enqueue_two():
-#     shelter = Queue()
+def test_enqueue_two():
+    shelter = Queue()
 
-#     shelter.enqueue({'type':'dog', 'name':'fido'})
-#     shelter.enqueue({'type':'cat', 'name':'mittens'})
+    shelter.enqueue({'type':'dog', 'name':'fido'})
+    shelter.enqueue({'type':'cat', 'name':'mittens'})
 
-#     assert shelter.tail.value['type'] == 'cat'
+    assert shelter.tail.value['type'] == 'cat'
 
-# def test_enqueue_three():
-#     shelter = Queue()
+def test_enqueue_three():
+    shelter = Queue()
 
-#     shelter.enqueue({'type':'dog', 'name':'fido'})
-#     shelter.enqueue({'type':'cat', 'name':'mittens'})
-#     shelter.enqueue({'type':'dog', 'name':'bud'})
+    shelter.enqueue({'type':'dog', 'name':'fido'})
+    shelter.enqueue({'type':'cat', 'name':'mittens'})
+    shelter.enqueue({'type':'dog', 'name':'bud'})
 
-#     assert shelter.tail.value['name'] == 'bud'  
-# def test_dequeue_one():
-#     shelter = Queue()
+    assert shelter.tail.value['name'] == 'bud'  
+def test_dequeue_one():
+    shelter = Queue()
 
-#     shelter.enqueue({'type':'dog', 'name':'fido'})
-#     shelter.enqueue({'type':'cat', 'name':'mittens'})
-#     shelter.enqueue({'type':'dog', 'name':'bud'})
+    shelter.enqueue({'type':'dog', 'name':'fido'})
+    shelter.enqueue({'type':'cat', 'name':'mittens'})
+    shelter.enqueue({'type':'dog', 'name':'bud'})
 
-#     assert  shelter.dequeue('cat') == 'mittens'     
+    assert  shelter.dequeue('cat') == 'mittens'     
 
-# def test_dequeue_two():
-#     shelter = Queue()
+def test_dequeue_two():
+    shelter = Queue()
 
-#     shelter.enqueue({'type':'dog', 'name':'fido'})
-#     shelter.enqueue({'type':'cat', 'name':'mittens'})
-#     shelter.enqueue({'type':'dog', 'name':'bud'})
-#     # import pdb; pdb.set_trace()
-#     shelter.dequeue('cat')
+    shelter.enqueue({'type':'dog', 'name':'fido'})
+    shelter.enqueue({'type':'cat', 'name':'mittens'})
+    shelter.enqueue({'type':'dog', 'name':'bud'})
+    # import pdb; pdb.set_trace()
+    shelter.dequeue('cat')
 
-#     assert  shelter.dequeue('dog') == 'fido'  
+    assert  shelter.dequeue('dog') == 'fido'  
 
 def test_dequeue_three():
     shelter = Queue()
